c34549d1a5616eacd7a9dbd0cfaa45a608a4920f
isym444 <samir.yep@gmail.com>
2024-03-20 21:09:30 +0000

    docker try 2

--- CatProject/settings.py
+++ CatProject/settings.py
@@ -15,10 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
-STATIC_URL = "/static/"
-STATIC_ROOT = "static/"
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Quick-start development settings - unsuitable for production
